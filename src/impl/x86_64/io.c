8b79494b691893cd33113ab6488ff4ad186c4fd0
juppmax <140436800+juppmax@users.noreply.github.com>
2024-09-05 08:46:58 +0200

    jaaaaaaa

--- src/impl/x86_64/io.c
+++ src/impl/x86_64/io.c
@@ -105,3 +105,14 @@ static inline void wrmsr(uint64_t msr, uint64_t value){
         : "c"(msr), "a"(low), "d"(high)
     );
 }
+
+//Read a 64-bit value to a MSR
+static inline uint64_t rdmsr(uint64_t msr){
+    uint32_t low, high;
+    asm volatile (
+        "rdmsr"
+        : "=a"(low), "=d"(high)
+        : "c"(msr)
+    );
+	return ((uint64_t)high << 32) | low;
+}
